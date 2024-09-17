05670e8541e3f853f1a67be4ca5a8d45f76beb84
Gisela Soledad Hurtado <hurtadogiselasoledad@gmail.com>
2024-09-17 12:48:00 +0200

    spring boot created, mockito and hamcrest added

---
+++ src/main/java/org/factoriaf5/pizzeriapaca/PizzeriapacaApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.pizzeriapaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriapacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriapacaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/pizzeriapaca/PizzeriapacaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.pizzeriapaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriapacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
